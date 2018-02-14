13c4d1cc9b704c9aff3d53d408500cbe85d36853
Davide Monari <davide.monari@kuleuven.be>
2018-02-14 18:41:43 +0100

    test

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ var samlStrategy = new saml.Strategy({
   issuer: process.env.ISSUER,
   identifierFormat: null,
   // Service Provider private key
-  decryptionPvk: privateCert2,	// SP metadata will not show certificate if decryptionPvk not existing
+  decryptionPvk: privateCert,	// SP metadata will not show certificate if decryptionPvk not existing
   // Service Provider Certificate
   //privateCert: privateCert, // needs START END headers
   // Identity Provider's public key
