f64015b83e2c967c48d2a6dad4642828dad302aa
Davide Monari <davide.monari@kuleuven.be>
2018-02-19 19:30:24 +0100

    test

--- passport-saml/lib/passport-saml/saml.js
+++ passport-saml/lib/passport-saml/saml.js
@@ -521,12 +521,14 @@ SAML.prototype.validateSignatureForCert = function (signature, cert, fullXml, cu
   //   are validating, so if we see anything else, reject.
   if (sig.references.length != 1 )
     return false;
+  console.log("blocks here 1")
   var refUri = sig.references[0].uri;
   var refId = (refUri[0] === '#') ? refUri.substring(1) : refUri;
   // If we can't find the reference at the top level, reject
   var idAttribute = currentNode.getAttribute('ID') ? 'ID' : 'Id';
   if (currentNode.getAttribute(idAttribute) != refId)
     return false;
+  console.log("blocks here 2")
   // If we find any extra referenced nodes, reject.  (xml-crypto only verifies one digest, so
   //   multiple candidate references is bad news)
   var totalReferencedNodes = xpath(currentNode.ownerDocument,
