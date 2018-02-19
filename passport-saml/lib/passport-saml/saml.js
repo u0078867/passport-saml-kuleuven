7dad2bd4e7cce1702d245c33d519ac5bdeac74e8
Davide Monari <davide.monari@kuleuven.be>
2018-02-19 19:33:00 +0100

    test

--- passport-saml/lib/passport-saml/saml.js
+++ passport-saml/lib/passport-saml/saml.js
@@ -519,6 +519,7 @@ SAML.prototype.validateSignatureForCert = function (signature, cert, fullXml, cu
   sig.loadSignature(signature);
   // We expect each signature to contain exactly one reference to the top level of the xml we
   //   are validating, so if we see anything else, reject.
+  console.log("blocks here 1")
   if (sig.references.length != 1 )
     return false;
   console.log("blocks here 1")
@@ -526,6 +527,7 @@ SAML.prototype.validateSignatureForCert = function (signature, cert, fullXml, cu
   var refId = (refUri[0] === '#') ? refUri.substring(1) : refUri;
   // If we can't find the reference at the top level, reject
   var idAttribute = currentNode.getAttribute('ID') ? 'ID' : 'Id';
+  console.log("blocks here 2")
   if (currentNode.getAttribute(idAttribute) != refId)
     return false;
   console.log("blocks here 2")
