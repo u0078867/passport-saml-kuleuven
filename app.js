934dad8cef037d4c5f8e52a1b0e991f8b2168a17
Davide Monari <davide.monari@kuleuven.be>
2018-02-21 19:46:43 +0100

    test

--- app.js
+++ app.js
@@ -129,7 +129,7 @@ app.use(function(err, req, res, next) {
   //console.log("Fatal error: " + JSON.stringify(err));
   //next(err);
   //res.set('Content-Type', 'text/xml');
-  res.send(err.stack.slice(7))
+  res.status(500).send(err)
 });
 
 var server = app.listen(process.env.PORT, function () {
